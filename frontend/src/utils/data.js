9f30002ace9f15983b9a709cf5dd8e3435011d48
Alex Fedoroff <alexanderfedorov@ALEXANDERs-Mac-mini.local>
2023-02-08 20:04:21 +0300

    diff_fixes

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -1,10 +1,8 @@
 import infoImgFailure from '../images/AuthImgFailure.png';
 import infoImgSuccess from '../images/AuthImgSuccess.png';
 
-// export const
-// apiSettings = { address: `${window.location.protocol}${process.env.REACT_APP_API_URL}` };
-
-export const apiSettings = { address: 'http://localhost:2800' };
+export const apiSettings = { address: `${window.location.protocol}${process.env.REACT_APP_API_URL}` };
+// export const apiSettings = { address: 'http://localhost:2800' };
 
 export const infoTooltipSettings = {
   regFailureImg: infoImgFailure,
